8f744f92ff0f2f2b8543e87c42e8f6efbd4f576c
Ivan <77786271+IvanBuzduha@users.noreply.github.com>
2024-03-25 21:00:00 +0200

    change swiper

--- src/components/Slider/SliderSwiper.jsx
+++ src/components/Slider/SliderSwiper.jsx
@@ -2,10 +2,11 @@ import "./style.css";
 // import Swiper from "swiper/bundle";
 // import "./script";
 
-import "swiper/css/bundle";
+// import "swiper/css/bundle";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-
+import { Navigation } from "swiper/modules";
+import "swiper/css/navigation";
 const SliderSwiper = () => {
   // let swiper = new Swiper(".mySwiper", {
   //   spaceBetween: 30,
@@ -26,20 +27,30 @@ const SliderSwiper = () => {
 
   return (
     <Swiper
+      className={"mySwiper"}
       spaceBetween={30}
       slidesPerView={1}
-      navigation
+      navigation={true}
+      modules={[Navigation]}
       pagination={{ clickable: true }}
       onSlideChange={() => console.log("slide change")}
       onSwiper={swiper => console.log(swiper)}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <div className="swiper-button-next" />
-      <div className="swiper-button-prev" />
-      ...
+      <SwiperSlide>
+        <img src="./img/1140x641.jpg" alt="img1" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="./img/1140x641 (1).jpg" alt="img2" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="../img/1140x641 (2).jpg" alt="img3" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="../img/1140x641 (3).jpg" alt="img4" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="../img/05-659.avif" alt="img5" />
+      </SwiperSlide>
     </Swiper>
   );
 };
