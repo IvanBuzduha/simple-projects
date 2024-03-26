994504cd587a310a0620a3daadc56e3653f04b8a
Ivan <77786271+IvanBuzduha@users.noreply.github.com>
2024-03-26 19:53:20 +0200

    add

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 class HomePage extends Component {
   state = {};
   render() {
-    return <h1>Welcome</h1>;
+    return (
+      <>
+        <h1>Welcome</h1>
+        <p>These are simple projects for working out certain technologies and techniques.</p>;
+      </>
+    );
   }
 }
 
